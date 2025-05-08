7819c06ecfe03710d66355075b2d52e6d1b9a98a
VangelisKarasas <120601673+VangelisKarasas@users.noreply.github.com>
2025-05-08 23:12:51 +0300

    Create forms.py

---
+++ FlaskTimeIT/forms.py
@@ -0,0 +1,49 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from FlaskTimeIT.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists. Please try again')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists. Please try again')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class PostForm(FlaskForm):
+    contact_person = StringField('Customer', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    customer = StringField('Customer', validators=[DataRequired()])
+    debit_time = StringField('Debit Time')
+    submit = SubmitField('Post')
+
+
+class InvoiceForm(FlaskForm):
+    contact_person = StringField('Customer', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    customer = StringField('Customer', validators=[DataRequired()])
+    debit_time = StringField('Debit Time')
+    submit = SubmitField('Post')
